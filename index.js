27055df2ab3a67c57d5a6a5c65ea9d622818a094
sophiazheli07 <zelihovskaasofia5@gmail.com>
2022-12-06 21:56:29 +0200

    done

--- index.js
+++ index.js
@@ -1,15 +1,99 @@
-var sunday = 0;
-var monday = 1;
-var tuesday = 2;
-var wednesday = 4;
-var thursday = 5;
-var friday = 6;
-var saturday = 7;
+var h=+prompt("hours");
+var m=+prompt("minutes");
+var s=+prompt("seconds");
+var date=new Date();
+if (typeof(h)=="number" && typeof(m)=="number" && typeof(s)=="number"){
+    date.setHours(h,m,s)
+    // alert(date)
+}
+else alert("undefined")
+    
+if((h>0&&h<24)&&(m>=0&&m<60)&&(s>=0&&s<60))
+    {   
+        date.setHours(h,m,s);     
+        alert(date); 
+    }
+else alert("undefined data")
 
-var x=parseInt(prompt("enter number of the day"))
-if(x>=1 && x<6) { alert("weekday")}
-else if(x==6){ alert("saturday")}
-else {alert("sunday")}
+
+
+
+// var n=+prompt("enter your age");
+// // if(n>120||n<0) alert("undefined age")
+// if(n<0){
+//     n=-n; //modul
+//     alert(n);
+// }
+// else alert(n)
+
+
+
+// var n=parseInt(prompt())
+// var n=+prompt("enter number"); //same as parseInt
+// if(n<0) alert("less than 0");
+// if(n>0) alert("more than 0");
+// else alert("zero")
+
+
+
+// var value = prompt('Какого цвета ваши глаза','');
+// if (value == 'голубые') {alert('Мне нравятся голубые глаза');}
+// else if (value == 'зеленые') {alert('Зеленый - мой любимый цвет!');}
+// else if (value == 'карие') {alert('У моего отца карие глаза');}
+// else if (value == 'серые') {alert('У меня тоже серые глаза');}
+// else {alert('У вас и вправду такие глаза?');}
+
+
+
+// var value = prompt('Какого цвета ваши глаза?','');
+// switch (value) {
+// case 'голубые' : 
+// alert('Мне нравятся голубые глаза'); 
+// break;
+// case 'зеленые' : 
+// alert('Зеленый - мой любимый цвет!'); 
+// break;
+// case 'карие' : 
+// alert('У моего отца карие глаза'); 
+// break;
+// case 'серые' : 
+// alert('У меня тоже серые глаза');
+// break;
+// default: alert('У вас и вправду такие глаза?');
+// }
+
+
+
+// var candy=3;
+// switch(candy)
+// {
+// case 1:
+//     alert("one candy");
+// break;
+// case 2:
+
+
+// case 3:
+//     alert("two or three candies");
+// break;
+// default:
+//     // alert("i have idea bro")
+//     document.write("i have idea bro")
+// }
+// var sunday = 0;
+// var monday = 1;
+// var tuesday = 2;
+// var wednesday = 4;
+// var thursday = 5;
+// var friday = 6;
+// var saturday = 7;
+
+
+// var x=parseInt(prompt("enter number of the day"))
+// if(x>=1 && x<6) { alert("weekday")}
+// else if(x==6){ alert("saturday")}
+// else {alert("sunday")}
+
 
 
 // var x =parseInt(prompt("enter X"));
@@ -22,11 +106,13 @@ else {alert("sunday")}
 // else alert("mistake")
 
 
+
 // if(10<5){
 //     alert("true");
 //      alert("true1");
 //       alert("true3");
 // }
+
 
 
 
@@ -41,6 +127,8 @@ else {alert("sunday")}
 // {alert("Это взрослый");}
 
 // }
+
+
 
 
 
