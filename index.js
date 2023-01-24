f8a215b5a29ba5d1f25542f1fd183f4ae2423d28
sophiazheli07 <zelihovskaasofia5@gmail.com>
2023-01-24 19:35:06 +0200

    save

---
+++ index.js
@@ -0,0 +1,64 @@
+// const person = {
+//     name1: "Alex",
+//     age: 10,
+//     email: "email@gmail.com",
+// };
+
+// const person1 = {
+//     name1: "Alex",
+// }
+
+// person.name1
+// person.age
+// person.email
+
+// const {name1, age, email} = person;
+// console.log(name1, age, email, person);
+
+// Object.freeze(person);
+
+// delete person.age;
+
+// person.city = "Kyiv";
+// console.log(person);
+
+// console.log(Object.isFrozen(person1));
+
+// const data = {
+//     name1: "Alex",
+//     age: 15,
+//     city: "Kyiv",
+// };
+
+// const data1 = {
+//     name1: "Alex",
+//     age: 15,
+//     city: "Kyiv",
+// };
+
+// Object.freeze(data);
+
+// data.email = "email@gmail.com"
+
+// console.log(data)
+
+// data1.email = "email@gmail.com"
+
+// console.log(data1)
+
+
+
+const object = {
+    person1: {
+        name: "Igor",
+        age: 20
+    },
+    person2: {
+        name: "Oleg",
+        age: 50
+    },
+    person3: {
+        name: "Alex",
+        age: 40
+    },
+}
