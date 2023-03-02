720de11101ebbc0e86fdd6c5ae03986c78c26f74
sophiazheli07 <zelihovskaasofia5@gmail.com>
2023-03-02 22:00:34 +0200

    done

---
+++ index.js
@@ -0,0 +1,48 @@
+const nameInput = document.querySelector("#name");
+const ageInput = document.querySelector("#age");
+const createButton = document.querySelector("#create");
+const usersSection = document.querySelector("#users-section");
+
+let users = [];
+
+const deleteUser = (inexOfUsers) => {
+    users = users.filter((el, i) => i !== inexOfUsers);
+    renderUsers();
+}
+
+function renderUsers () {
+
+    usersSection.innerHTML = "";
+
+    const usersContent = users.map((user) => `<div class="user-card">
+        <p>${user.name}</p>
+        <span>${user.age}</span>
+        <button class="delete-user-button">Remove</button>
+    </div>`);
+
+    usersContent.forEach((userLayout) => {
+        usersSection.innerHTML +=userLayout
+    });
+
+    const dleeteButton = [...document.querySelectorAll(".delete-user-button")];
+
+    dleeteButton.forEach((el, i) =>{
+        el.onclick = () => deleteUser(i);
+    })
+}
+
+
+createButton.onclick = () => {
+    const name = nameInput.value;
+    const age = +ageInput.value;
+
+    const user = {name: name, age: age};
+    users.push(user);
+    
+    nameInput.value = "";
+    ageInput.value = "";
+
+    renderUsers();
+}
+
+// function declaration функція замість константи достпупна всюди до і після її написання renderUsers();
