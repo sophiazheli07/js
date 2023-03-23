50f2b96e7536de7f6bed6e353228184440776d1b
sophiazheli07 <zelihovskaasofia5@gmail.com>
2023-03-23 18:59:58 +0200

    initial

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ function fillPromises (n) {
 
     for(let i = 0; i < n; i++) {
         const promise = new Promise((resolve, reject) => {
-            const timeOfExecution =i*1000;
+            const timeOfExecution = i*1000;
             console.log(timeOfExecution);
             setTimeout(() => {
                 resolve(timeOfExecution);
@@ -55,3 +55,80 @@ function fillPromises (n) {
 
 Promise.race(fillPromises(4))
 .then((timeOfExecution) => console.log(`Time:`, timeOfExecution))
+
+//LocalStorage
+//Session Storage
+
+//JSON => string
+
+// localStorage.setItem("msg", "hello");
+// localStorage.setItem("num", 1);
+// localStorage.setItem("boolean", true);
+// localStorage.setItem("array", [1,2,3,4,5,6]);
+// localStorage.setItem("bigData", JSON.stringify({name: "Igor", array: [1,2,3,4]}));
+
+// const user = {name: "Oleg"}
+
+// // console.log(JSON.stringify(user));
+
+// localStorage.setItem("user", JSON.stringify(user));
+
+// const msg = localStorage.getItem("msg");
+// const boolean = localStorage.getItem("boolean");
+
+// const userData = JSON.parse(localStorage.getItem("user"));
+// const array = JSON.parse(localStorage.getItem("array"));
+// const bigData = JSON.parse(localStorage.getItem("bigData"))
+// console.log(msg, boolean, userData, array, bigData)
+
+const users = localStorage.getItem("users") ? JSON.parse(localStorage.getItem("users")) : [];
+
+if (users.length === 0){
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => response.json())
+    .then(users => {
+      console.log(users, "users");
+      localStorage.setItem("users", JSON.stringify(users))
+    });
+} else {
+    console.log(users, "users")
+}
+
+sessionStorage.setItem("name", "Igor")
+
+const emailField = document.querySelector("#text");
+const passwordField = document.querySelector("#password");
+const submitBtn = document.querySelector("#submit");
+
+const userEmail = document.querySelector("#user-email");
+const userPassword = document.querySelector("#user-password");
+
+emailField.oninput = (e) => {
+    console.log("email", e.target.value)
+}
+passwordField.oninput = (e) => {
+    console.log("password", e.target.value)
+}
+submitBtn.onclick = (user) => {
+    const email = emailField.value;
+    const password = passwordField.value;
+    user = {email, password};
+    userEmail.textContent = email;
+    userPassword.textContent = password;
+
+
+    emailField.value = "";
+    passwordField.value = "";
+
+    localStorage.setItem("user", JSON.stringify(user))
+}
+
+window.onclose = () => {
+
+}
+
+
+
+
+// localStorage.setItem("users", JSON.stringify(users));
+// console.log(users)
