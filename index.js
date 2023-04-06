58c2d78d4178be165b6c99dbfa480e3258978177
sophiazheli07 <zelihovskaasofia5@gmail.com>
2023-04-06 19:27:27 +0300

    initial

--- index.js
+++ index.js
@@ -62,6 +62,7 @@ async function fetchTodo() {
   const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
   const json = await response.json();
   console.log(json);
+  return json
 }
 
 fetchTodo();
