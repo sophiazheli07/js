a5196ea065ff2c1d38fbc5ed3ec3b1fb79248ce2
sophiazheli07 <zelihovskaasofia5@gmail.com>
2023-05-30 22:01:59 +0300

    initial

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ class App {
   static API_KEY = "bb71f6d0a5msh8bea47b10b6fd85p1e95cbjsnac5a7c3dc7df";
   static SEARCH_URL =
     "https://online-movie-database.p.rapidapi.com/auto-complete?q=";
+
   static DEFAULT_HEADERS = {
-    headers: {
-      "X-RapidAPI-Key": App.API_KEY,
-      "X-RapidAPI-Host": "online-movie-database.p.rapidapi.com",
-    },
+    "X-RapidAPI-Key": App.API_KEY,
+    "X-RapidAPI-Host": "online-movie-database.p.rapidapi.com",
   };
+
   static button = document.querySelector("button");
-  static input = document.querySelector("#input");
-  static output = document.querySelector("#output");
+  static input = document.querySelector("input");
+  static output = document.querySelector("section");
+  static watchListOutput = document.querySelector("#watch-list");
 
   constructor(data = [], watchList = []) {
     this.data = data;
@@ -20,13 +21,15 @@ class App {
   }
 
   onInputChange() {}
+
+  //start search
   onButtonClick() {
-    console.log(App.input.value)
+    console.log(App.input.value);
     this.getDataBySearch(App.input.value).then(() => {
-        App.input.value = "";
+      App.input.value = "";
       console.log("Data", this.data);
 
-      this.renderData(this.data, App.output)
+      this.renderData(this.data, App.output);
     });
   }
 
@@ -39,12 +42,19 @@ class App {
 
       this.data = data.d ? data.d : [];
     } catch (e) {
-      alert("Query is invelid");
+      console.log(e, "error");
+      alert("Query is invalid");
     }
+  }
+  addWachListData(film) {
+    const oldWatchList = JSON.parse(localStorage("watchList") || "[]");
+    localStorage.setItem("watchList", [...oldWatchList, film]);
+
   }
 
   renderData(dataToRender, outputElement = App.output) {
     outputElement.innerHTML = "";
+
     dataToRender.forEach((film) => {
       const {
         id,
@@ -56,21 +66,74 @@ class App {
         y: year,
         i: imageData,
       } = film;
-      const { imageUrl, width, height } = imageData || {imageUrl: "", width: 1000, height: 1000};
+      const { imageUrl, width, height } = imageData || {
+        imageUrl:
+          "https://static.vecteezy.com/system/resources/previews/007/160/087/original/video-icon-video-symbol-play-video-sign-free-vector.jpg",
+        width: 1000,
+        height: 1000,
+      };
 
       outputElement.innerHTML += `
-      <div class="film-el">
-      <img src="${imageUrl}" width="${width / 10}" height="${height / 10}"/>
-      <h3>${title}</h3>
-      <span>${rank}</span>
-      <span>${actors}</span>
-      <span>${year}</span>
-
-      <div/>
+        <div class="film-elem">
+        <img src="${imageUrl}" />
+        <h3>${title}</h3>
+        <span>Rating: ${rank}</span>
+        <span>Actors: ${actors}</span>
+        <span>Year: ${year || "unknown"}</span>
+        <button class = "film">Watch Later</button>
+        
+      </div>
+      
       `;
+    
+      
+      const filmButtons = document.querySelectorAll(".film");
+      [...filmButtons].forEach((btn, i) => {
+        btn.onclick = () => {
+          console.log("!!!!");
+          this.addWachListData(dataToRender[i]);
+        }
+        removeWatchListData(id) {
+
+          const oldWatchList = JSON.parse(this.getWatchListData());
+          
+          localStorage.setItem("watchList", JSON.stringify([...oldWatchList].filter((el) => el.id !== id)));
+          
+        };
+
+      })
+
+
+
     });
-  }
-  renderWatchList() {}
+
+  
+}
+checkIfWatchListContainsFilm() {
+  return this.getWatchListData().find((film) => film.id === id) ? true : false
+}
+
+renderWatchList() {
+App.output.style.visibility = "hidden";
+App.watchListOutput.style.visibility = 
+}
 }
 
 new App();
+
+
+  // onWatchLaterClick() {
+
+  //   var movieId = 'your_movie_id';
+
+  //   if (this.watchList.indexOf(movieId) === -1) {
+  //     this.watchList.push(movieId);
+
+  //     localStorage.setItem('watchLaterList', JSON.stringify(this.watchList));
+
+  //     alert("Added");
+  //   } else {
+
+  //     alert("It's already here");
+  //   }
+  // }
