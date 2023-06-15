275c36e0d564f24635721330b23dfb08b1164515
sophiazheli07 <zelihovskaasofia5@gmail.com>
2023-06-15 22:09:45 +0300

    initial-Angular

--- NodeJS/fisrt-project/htmlConstructor.mjs
+++ NodeJS/fisrt-project/htmlConstructor.mjs
@@ -1,19 +1,49 @@
-import fs from "fs"
+import fs from "fs";
+
 export class HTMLConstructor {
+  constructor(fileName = "", content = "", style = "") {
+    this.fileName = fileName;
+    this.content = content;
+    this.style = style;
+  }
 
-    constructor (fileName = "", content = "") {
-        this.fileName = fileName;
-        this.content = content;
+  generateBasicHTMLContent(content, style) {
+    return `
+    <html>
+        <head>
+        <style>
+            ${style}
+        </style>
+        </head>
+        <body>
+            ${content}
+        </body>
+    </html>
+    `
+  }
+
+  generateFoldersFromPath(path) {
+    if (path.includes("/")) return path;
+
+    const pathItems = path.split("/");
+
+    pathItems.forEach((p, i) => {
+      const fullPathToCurrentFolder = pathItems.slice(0, i + 1).join("/");
+      console.log(fullPathToCurrentFolder, "!!!!!!!!!!!!!!!!!!!!!!!");
+      !fs.existsSync(fullPathToCurrentFolder) && fs.mkdirSync(fullPathToCurrentFolder);
+    });
+  }
+
+  generateFile(path = "") {
+    if (path) {
+      this.generateFoldersFromPath(path);
     }
 
-    generateFIle(path = "/htmlContent") {
-        if (path) {
-            !fs.exsistsSync(path) && fs.mkdirSync(path);
-        }
+    fs.writeFileSync(
+      path ? `${path}/${this.fileName}.html` : `${this.fileName}.html`,
+      this.generateBasicHTMLContent(this.content, this.style)
+    );
 
-        fs.writeFileSync(`${path}/${this.fileName}.html`, this.content);
-        
-        return this;
-
-    }
+    return this;
+  }
 }
--- NodeJS/fisrt-project/index.mjs
+++ NodeJS/fisrt-project/index.mjs
@@ -4,27 +4,39 @@ import { HTMLConstructor } from "./htmlConstructor.mjs";
 
 console.log(
   countryCodes.all().find((country) => country.countryCode === "UA"),
-  "!!!!!"
+  "!!!"
 );
 
 const data = JSON.parse(fs.readFileSync("./posts.json"));
 
 function main(data) {
-    //   console.table(data);
-    const countriesLayouts = countryCodes
-      .all()
-      .slice(0, 10)
-      .map(
-        (country) => `<div>
-          <p>${country.countryNameEn}</p>
-          <p>${country.region}</p>
-          <p>${country.officialLanguageNameEn}</p>
-      </div>`
-      );
-  
-    console.log(countriesLayouts, "!!!");
-  
-    const htmlConstructor = new HTMLConstructor("countries.html");
-  }
+  //   console.table(data);
+  const countriesLayouts = countryCodes
+    .all()
+    .slice(0, 10)
+    .map(
+      (country) => `<div>
+        <p>${country.countryNameEn}</p>
+        <p>${country.region}</p>
+        <p>${country.officialLanguageNameEn}</p>
+    </div>`
+    );
+
+  const htmlConstructor = new HTMLConstructor(
+    "countries",
+    countriesLayouts.join(""),
+    `
+    div {
+      boder: 1px solid black;
+      margin: 5px;
+      padding: 5px;
+      color: red;
+    }
+    `
+  );
+
+  htmlConstructor.generateFile("html");
+  // htmlConstructor.generateFile("");
+}
 
 main(data);
