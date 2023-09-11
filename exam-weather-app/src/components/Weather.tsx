a67946046d8a1f0df24e6b12db0472a15868f06b
User <User@MacBook-Pro-4.local>
2023-09-11 22:57:15 +0300

    final-push

--- exam-weather-app/src/components/Weather.tsx
+++ exam-weather-app/src/components/Weather.tsx
@@ -5,15 +5,34 @@ import {
   FlexComponent,
   Input,
   IconHolder,
-  Button
+  Button,
+  Span,
+  SpanMain,
+  TimeP,
+  TimeDiv,
 } from "../services/styled";
 import { FaWind } from "react-icons/fa";
-import { WiDaySunny, WiDayRain, WiCloudy } from 'react-icons/wi';
+import { WiDaySunny, WiDayRain, WiCloudy } from "react-icons/wi";
+import { FaTemperatureHigh } from "react-icons/fa";
+import { PiWindBold } from "react-icons/pi";
+import { WiHumidity } from "react-icons/wi";
+import { FaTemperatureLow } from "react-icons/fa";
 
 const Weather = () => {
   const [weatherData, setWeatherData] = useState<any>(null);
   const [location, setLocation] = useState("");
   const [loading, setLoading] = useState(false);
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+  }, []);
+
+  const convertToFahrenheit = (fahrenheit: number) => {
+    return Math.round(((fahrenheit - 32) * 5) / 9);
+  };
 
   const apiKey = "16559c5f6amsh3dd37f0e2c50eeap167a45jsn9df86031c244";
 
@@ -26,7 +45,6 @@ const Weather = () => {
       setLoading(true);
 
       const apiUrl = `  https://yahoo-weather5.p.rapidapi.com/weather?location=${location}&format=json&u=f`;
-    
 
       const response = await fetch(apiUrl, {
         method: "GET",
@@ -54,12 +72,12 @@ const Weather = () => {
 
   const getWeatherIcon = (condition: string) => {
     switch (condition.toLowerCase()) {
-      case 'sunny':
-        return <WiDaySunny size='60px'/>;
-      case 'rainy':
-        return <WiDayRain size='60px'/>;
-      case 'cloudy':
-        return <WiCloudy size='60px'/>;
+      case "sunny":
+        return <WiDaySunny size="60px" />;
+      case "rainy":
+        return <WiDayRain size="60px" />;
+      case "cloudy":
+        return <WiCloudy size="60px" />;
       default:
         return null;
     }
@@ -69,8 +87,14 @@ const Weather = () => {
     <GlobalStyles>
       <FlexComponent>
         <IconHolder>
-            <FaWind size='20px' color='#5b759a'/>
-            </IconHolder>
+          <FaWind size="20px" color="#5b759a" />
+        </IconHolder>
+        <TimeDiv>
+          <TimeP>
+            Kyiv <br /> Time
+          </TimeP>
+          <TimeP>{currentTime.toLocaleTimeString()}</TimeP>
+        </TimeDiv>
       </FlexComponent>
 
       <FlexComponent>
@@ -83,33 +107,78 @@ const Weather = () => {
           />
           <Button type="submit">Search</Button>
         </form>
-        
+
         {loading && <p>Loading...</p>}
         {weatherData && (
           <div>
-          <h2>Weather for {location}</h2>
-          <div>{weatherData && getWeatherIcon(weatherData.current_observation.condition.text)}</div>
-          <p>Temperature: {weatherData.current_observation.condition.temperature}°F</p>
-          <p>Wind: {weatherData.current_observation.wind.speed} kph</p>
-          <p>Humidity: {weatherData.current_observation.atmosphere.humidity}%</p>
-          
-        </div>
+            <h2>Weather for {location}</h2>
+            <div>
+              {weatherData &&
+                getWeatherIcon(weatherData.current_observation.condition.text)}
+            </div>
+
+            <p>
+              <FaTemperatureHigh
+                style={{ marginRight: "10px", height: "30px", width: "30px" }}
+              />
+              {convertToFahrenheit(
+                weatherData.current_observation.condition.temperature
+              )}{" "}
+              °C
+            </p>
+            <p>
+              <PiWindBold
+                style={{ marginRight: "10px", height: "30px", width: "30px" }}
+              />
+              {weatherData.current_observation.wind.speed} kph
+            </p>
+            <p>
+              <WiHumidity
+                style={{ marginRight: "10px", height: "30px", width: "30px" }}
+              />
+              {weatherData.current_observation.atmosphere.humidity}%
+            </p>
+          </div>
         )}
-        </FlexComponent>
-        <FlexComponent>
+      </FlexComponent>
+      <FlexComponent>
         {weatherData && (
           <div>
-            <h2>7-Day Forecast</h2>
-            <ul>
-              {weatherData.forecasts.slice(0, 7).map((forecast: any) => (
-                <li key={forecast.date}>
-                  <span>{forecast.day}</span>
-                  <span>{forecast.text}</span>
-                  <span>High: {forecast.high}°F</span>
-                  <span>Low: {forecast.low}°F</span>
-                </li>
-              ))}
-            </ul>
+            <Title>7-Day Forecast</Title>
+            {weatherData.forecasts.slice(0, 7).map((forecast: any) => (
+              <div key={forecast.date}>
+                <SpanMain>{forecast.day}</SpanMain>
+                <br />
+
+                <Span>
+                  <FaTemperatureHigh
+                    style={{
+                      marginRight: "10px",
+                      height: "20px",
+                      width: "20px",
+                      marginBottom: "-5px",
+                      marginLeft: "10px",
+                    }}
+                  />{" "}
+                  {convertToFahrenheit(forecast.high)}°C
+                </Span>
+                <Span>
+                  <FaTemperatureLow
+                    style={{
+                      marginRight: "10px",
+                      height: "20px",
+                      width: "20px",
+                      marginBottom: "-5px",
+                      marginLeft: "10px",
+                    }}
+                  />{" "}
+                  {convertToFahrenheit(forecast.low)}°C
+                </Span>
+                <hr
+                  style={{ border: "2px solid #0B121E", borderRadius: "2px" }}
+                />
+              </div>
+            ))}
           </div>
         )}
       </FlexComponent>
--- exam-weather-app/src/services/styled.tsx
+++ exam-weather-app/src/services/styled.tsx
@@ -3,7 +3,8 @@ import styled from "styled-components";
 
 export const Title = styled.h1`
   font-size: 36px;
-  color: #333;
+  color: white;
+  text-align: center;
 `;
 export const FlexComponent = styled.div`
   background-color: #202B3C;
@@ -21,7 +22,6 @@ export const IconHolder = styled.div`
   justify-content: center;
   text-align: center;
   align-items: center;
-
   padding-top: 5px;
   padding-left: 3px;
   padding-right: 3px;
@@ -37,6 +37,22 @@ export const Input = styled.input`
   padding: 5px;
   margin: 5px;
 `;
+export const Span = styled.span`
+margin-top: 10px;
+  margin: 5px;
+
+`;
+export const SpanMain = styled.span`
+  margin-top: 10px;
+  text-transform: uppercase;
+  font-weight: 700;
+
+`;
+export const TimeDiv = styled.div`
+   display: flex;
+  flex-direction: column;
+
+`;
 export const Button = styled.button`
   background-color: #0B121E;
   border: none;
@@ -49,13 +65,25 @@ export const Button = styled.button`
   margin-left: 5px
 `;
 
+export const Hr = styled.hr`
+  margin-top: 10px;
+  background-color: black;
+`;
+export const TimeP = styled.p`
+  all: unset;
+  font-size: 15px;
+  margin-top: 20px;
+  font-weight: 600;
+`;
+
+
 export const GlobalStyles = styled.div`
     background-color: #0B121E;
     margin: 20px;
     font-family: Arial, sans-serif;
     border-radius: 35px;
     min-height: 400px;
-    margin-top: 200px;
+    margin-top: 100px;
     padding: 20px;
     font-size: 5px;
     display: flex;
